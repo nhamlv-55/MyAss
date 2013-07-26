9dad17bb81e6c0ce4d964991b3c83f75e724ad1e
nhamlv-55 <nhamlevan@gmail.com>
2013-07-26 22:19:05 +0900

    yolo

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.test;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.example.test.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +23,17 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    //to match a method to android:onClick
+    //must have a void return
+    //must have View as the only parameter
     
+    //Called when the user clicks the Find button
     public void find(View view){
-    	
+    	Intent intent=new Intent(this, DisplayMessageActivity.class);
+    	EditText editText=(EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
     
 }
